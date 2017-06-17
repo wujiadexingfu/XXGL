547b915e8d0cc0fed0f5b75ed7b3d78c6155f056
wujiadexingfu <1044193732@qq.com>
2017-06-17 14:13:16 +0800

    新增js的文件

---
+++ XXGL/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using XXGL.Base.IService;
+
+namespace XXGL.Controllers
+{
+    public class HomeController : Controller
+    {
+        IUsersService _userService{get;set;}
+        public HomeController(IUsersService userService)
+        {
+            _userService = userService;
+        }
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            var user = _userService.Login("admin", "系统管理员");
+            return View();
+        }
+    }
+}
